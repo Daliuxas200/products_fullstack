3e5b24259f6bc8642fdcab02e139860701a734b4
Dalius Slavickas <slavickasd@gmail.com>
2021-10-25 16:15:55 +0300

    feat: basic products markup and styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,41 @@ import './style.scss';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="page">
+      <section className="products">
+        <ul className="products__list">
+          <li className="products__item">
+            <figure>
+              <img src="" alt="" className="products__item__image" />
+            </figure>
+            <div className="products__item__details">
+              <h3 className="products__item__name">Some Products</h3>
+              <span className="products__item__brand">Prodactan</span>
+              <span className="products__item__price">99.87$</span>
+            </div>
+          </li>
+          <li className="products__item">
+            <figure>
+              <img src="" alt="" className="products__item__image" />
+            </figure>
+            <div className="products__item__details">
+              <h3 className="products__item__name">Some Products</h3>
+              <span className="products__item__brand">Prodactan</span>
+              <span className="products__item__price">99.87$</span>
+            </div>
+          </li>
+          <li className="products__item">
+            <figure>
+              <img src="" alt="" className="products__item__image" />
+            </figure>
+            <div className="products__item__details">
+              <h3 className="products__item__name">Some Products</h3>
+              <span className="products__item__brand">Prodactan</span>
+              <span className="products__item__price">99.87$</span>
+            </div>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
